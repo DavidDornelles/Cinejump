ac070dfa3c4c6bfad533ea09e28f98fc9170b3cf
David <Dornelles>
2021-12-14 01:55:11 -0300

    header (menu/navigation)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
 import Api from "./api";
+import './styles.css';
+import WhiteLogo from './assets/Logo-white.svg';
+import SearchIcon from './assets/FiSearch.svg';
+import UserIcon from './assets/FaRegUserCircle.svg';
+
+class App {
+  constructor() {
+    this.app = document.getElementById('app');
+    this.app.innerHTML = this.render();
+  }
+
+  render() {
+    const app = /*html*/`
+      <div class="bg-pink pa"></div>
+      <header class="header">
+        <nav class="navigation container">
+          <ul class="nav-items">
+            <li class="nav-item">Filmes</li>
+            <li class="nav-item">Séries</li>
+            <li class="nav-logo"><img src="${WhiteLogo}" /></li>
+            <li class="nav-search"><img src="${SearchIcon}" /></li>
+            <li class="nav-account"><img src="${UserIcon}" /></li>
+          </ul>
+        </nav>
+      </header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    `;
+    return app;
+  }
+}
 
 class Movies {
   constructor() {
@@ -10,6 +41,8 @@ class Movies {
     console.log('this.moviesList -> ', this.moviesList);
   }
 }
+
+const app = new App();
 
 const listMovies = new Movies();
 

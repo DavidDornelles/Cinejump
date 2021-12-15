0113bfff29855eccb17d7703977a82cced6b59b8
David <Dornelles>
2021-12-15 04:40:24 -0300

    movies list

--- src/main.js
+++ src/main.js
@@ -6,11 +6,43 @@ import UserIcon from './assets/FaRegUserCircle.svg';
 
 class App {
   constructor() {
+    this.moviesList = {
+      popular: this.getMovies('popular'),
+      playing: this.getMovies('now_playing'),
+    };
     this.app = document.getElementById('app');
-    this.app.innerHTML = this.render();
   }
 
+  async getMovies(category) {
+    if (category === 'popular') {
+      const { results } = await Api.getMoviesByCategory(category);
+      this.moviesList.popular = results;
+    } else {
+      const { results } = await Api.getMoviesByCategory(category);
+      this.moviesList.playing = results;
+    }
+    this.app.innerHTML = this.render();
+  }
+  
   render() {
+    const popular = this.moviesList.popular
+      .map(movie => /*html*/`
+        <article class="section-card pr">
+          <picture>
+            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="img-responsive" />
+          </picture>
+        </article>`)
+      .join('');
+
+    const playing = this.moviesList.playing
+      .map(movie => /*html*/`
+        <article class="section-card pr">
+          <picture>
+            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="img-responsive" />
+          </picture>
+        </article>`)
+      .join('');
+
     const app = /*html*/`
       <div class="bg-pink pa"></div>
       <header class="header">
@@ -25,20 +57,20 @@ class App {
         </nav>
         <div class="highlights container">
           <div class="primary pr">
-            <img src="https://missaohadomi.org/wp-content/uploads/2015/07/import_placeholder.png" />
+            <img src="https://missaohadomi.org/wp-content/uploads/2015/07/import_placeholder.png" class="img-responsive" />
             <div class="infos pa">
               <h3 class="infos-title">1917</h3>
               <p class="infos-text">Lightning McQueen, a hotshot rookie race car driven to succeed, discovers that life is about the journey, not the finish line, when he finds himself unexpectedly detoured in the sleepy Route</p>
             </div>
           </div>
           <div class="secondary pr">
-            <img src="https://missaohadomi.org/wp-content/uploads/2015/07/import_placeholder.png" />
+            <img src="https://missaohadomi.org/wp-content/uploads/2015/07/import_placeholder.png" class="img-responsive" />
             <div class="infos pa">
               <p class="infos-text">Carros 2</p>
             </div>
           </div>
           <div class="tertiary pr">
-            <img src="https://missaohadomi.org/wp-content/uploads/2015/07/import_placeholder.png" />
+            <img src="https://missaohadomi.org/wp-content/uploads/2015/07/import_placeholder.png" class="img-responsive" />
             <div class="infos pa">
               <p class="infos-text">Bad Boys 2</p>
             </div>
@@ -46,6 +78,18 @@ class App {
         </div>
       </header>
       <main>
+        <section class="section container">
+          <h2 class="section-title">Popular</h2>
+          <div class="section-content">
+            ${popular}
+          </div>
+        </section>
+        <section class="section container">
+          <h2 class="section-title">Em Exibição</h2>
+          <div class="section-content">
+            ${playing}
+          </div>
+        </section>
       </main>
       <footer>Footer</footer>
     `;
@@ -53,20 +97,4 @@ class App {
   }
 }
 
-class Movies {
-  constructor() {
-    this.moviesList = [];
-  }
-
-  async getMovies(category) {
-    this.moviesList = await Api.getMoviesByCategory(category);
-    console.log('this.moviesList -> ', this.moviesList);
-  }
-}
-
 const app = new App();
-
-const listMovies = new Movies();
-
-listMovies.getMovies('popular');
-listMovies.getMovies('latest');

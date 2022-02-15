949e1848252d6cf74108c34df56e56684135cbf9
David <Dornelles>
2022-02-15 05:15:00 -0300

    upcoming movie trailers

--- src/api.js
+++ src/api.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 class Api {
   static async getMoviesByCategory(category) {
-    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/${category}?api_key=5947bc2184fbd0743bf96b5f416de9e1`);
+    const { data: { results } } = await axios.get(`https://api.themoviedb.org/3/movie/${category}?api_key=5947bc2184fbd0743bf96b5f416de9e1`);
 
-    return data;
+    return results;
   }
-
+  
   static async getMovieById(id) {
     const { data } = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=5947bc2184fbd0743bf96b5f416de9e1`);
-
+    
     return data;
   }
-
+  
   static async getTrailerById(id) {
-    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=5947bc2184fbd0743bf96b5f416de9e1`);
+    const { data: { results } } = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=5947bc2184fbd0743bf96b5f416de9e1`);
 
-    return data;
+    return results[0].key;
   }
 }
 
